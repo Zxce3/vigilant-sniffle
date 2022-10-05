6afd5f07b7c9563248e019c6e7fa176df9856f34
Memet Zx <memetpea25@gmail.com>
2022-10-06 03:20:30 +0700

    initial commit
    Signed-off-by: Memet Zx <memetpea25@gmail.com>

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './_includes/**/*.liquid',
+    './_pages/**/*.liquid',
+    './_layouts/**/*.liquid',
+    './_posts/*.md',
+    './*.liquid',
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/typography'), require("daisyui")],
+  daisyui: {
+    themes: ["light", "dark", "luxury", "black", "dracula"],
+  },
+}
